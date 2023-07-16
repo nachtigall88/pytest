f37ad8ba088210adaa4936cc208627d2b2213c10
Max <udin@esmil.eu>
2023-07-16 14:49:39 +0300

    tests

--- tests/test_division_func.py
+++ tests/test_division_func.py
@@ -6,14 +6,12 @@ import pytest
                                                    (20, 10, 2),
                                                    (30, -3, -10),
                                                    (5, 2, 2.5)])
-def test_division_good(a, b, expected_result):
+def test_division_(a, b, expected_result):
     assert division(a, b) == expected_result
 
 
-def test_zero_division():
-    with pytest.raises(ZeroDivisionError):
-        division(10, 0)
-
-def test_type_error():
-    with pytest.raises(TypeError):
-        division(10, '2')
+@pytest.mark.parametrize('expected_exception, divider', [(ZeroDivisionError, 0),
+                                                         (TypeError, '2')])
+def test_division_with_error(expected_exception, divider):
+    with pytest.raises(expected_exception):
+        division(10, divider)
