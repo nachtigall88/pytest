da0670498e6330fd68fff6fd78641c227489ebea
Max <udin@esmil.eu>
2023-07-16 14:40:13 +0300

    tests

--- tests/test_division_func.py
+++ tests/test_division_func.py
@@ -1,1 +1,15 @@
 from utils import division
+import pytest
+
+
+@pytest.mark.parametrize('a, b, expected_result', [(10, 2, 5),
+                                                   (20, 10, 2),
+                                                   (30, -3, -10),
+                                                   (5, 2, 2.5)])
+def test_division_good(a, b, expected_result):
+    assert division(a, b) == expected_result
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        division(10, 0)
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
 def division(a, b):
     return a / b
 
-print(division(10,2))
