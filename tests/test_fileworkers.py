098c8e79b06758fdb290778cf573c2f307eeddb0
Max <udin@esmil.eu>
2023-07-17 12:35:17 +0300

    database testing

--- tests/test_fileworkers.py
+++ tests/test_fileworkers.py
@@ -1,5 +1,5 @@
 from my_funcs.file_workers import read_from_file
 
 def test_read_from_file():
-    test_data = ['one\n', 'two\n', 'three\n']
-    assert test_data == read_from_file('tests/testfile.txt')
+    test_data = ['one\n', 'two\n', 'three']
+    assert test_data == read_from_file('testfile.txt')
